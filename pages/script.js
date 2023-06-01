efcf67d109b7fea6196097f6016df8a367385318
TakayumAja <asraftakayuma76@gmail.com>
2023-06-01 10:16:10 +0700

    initial commit

---
+++ pages/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const navbarMenu = document.querySelector(".navbar-menu");
+  const navbarToggle = document.querySelector(".navbar-toggle");
+  navbarMenu.classList.toggle("active");
+  navbarToggle.classList.toggle("active");
+}
